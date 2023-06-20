909bf576d52c69e724d629f279dff53804ec7ad9
Manel <ea3eiz@ea3eiz.com>
2023-06-20 20:55:08 +0200

    Update cerrar_solodstar.sh

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -3,7 +3,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp AbrirsoloDstar.desktop /home/pi
 sleep 1
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x20 -e sudo sh ejecutar_solodstar.sh'" /home/pi/AbrirsoloDstar.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/; sudo sh ejecutar_solodstar.sh'" /home/pi/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR.png" /home/pi/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/pi/AbrirsoloDstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/status.ini
